01da2ba09888f62d316931f9a1f8b5f24f47aae7
DaviLuisSantos <83884291+DaviLuisSantos@users.noreply.github.com>
2021-05-12 11:58:25 -0300

    Update to the app.js with new resources

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,5 +1,13 @@
+const {
+    json
+} = require('express');
 const express = require('express');
 const app = express();
+
+app.use(json());
+app.use(express.urlencoded({
+    extended: false
+}));
 
 
 const users = [{ //Guarda todos os nome e idades no objeto "users"
@@ -16,9 +24,12 @@ const users = [{ //Guarda todos os nome e idades no objeto "users"
     }
 ];
 const posts = [{ //Guarda os títulos abaixo no objeto "posts"
-    title: 'My favorite foods'},
-    {title: 'My favorite games'
-}]
+        title: 'My favorite foods'
+    },
+    {
+        title: 'My favorite games'
+    }
+]
 
 
 
@@ -28,6 +39,11 @@ app.get('/', (req, res) => { //especifica um novo caminho
         user: {}
     });
 });
+app.post('/', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.status(201).send('Created User');
+})
 app.get('/users', (req, res) => {
     res.status(200).send(users); //mostra os usuários 
 });
@@ -56,6 +72,19 @@ app.get('/posts', (req, res) => {
 
 })
 
+app.post('/posts', (req, res) => {
+    const {
+        authorization
+    } = req.headers;
+    if (authorization && authorization === '123') {
+        const post = req.body;
+        console.log(post);
+        posts.push(post);
+        res.status(201).send(post);
+    } else {
+        res.status(403).send('Forbidden');
+    }
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
