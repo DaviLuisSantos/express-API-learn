a1ee08fce7ff6dc952e14cb075e5615931c964cc
DaviLuisSantos <83884291+DaviLuisSantos@users.noreply.github.com>
2021-05-13 19:51:12 -0300

    Monitoring Ram node code

---
+++ MonitorMem/status.js
@@ -0,0 +1,22 @@
+const { stat } = require('fs');
+const os = require('os');
+
+setInterval(()=>{
+    const { freemem, totalmem } = os;
+/*const freemem=os.freemem;  Poderia ser feito assim tbm
+const totalmem=os.totalmem;*/
+const total = parseInt(totalmem() / 1024 / 1024)
+const mem = parseInt(freemem() / 1024 / 1024)
+const percents = parseInt((mem / total) * 100)
+
+
+const stats = {
+    free: `${mem}MB`,
+    total: `${total}MB`,
+    usage: `${percents}%`
+}
+console.clear();
+console.log('=======PC STATS=======');
+console.table(stats);
+},1000)
+
